eff2de258955a3687969861c6d49ea8d701f31d5
Pedro Cursos <pedrocursos@icloud.com>
2018-05-21 00:10:05 +0200

    Archivos generados por Cocoapods después de instalar los pods

--- Cholor/Pods/Target Support Files/Pods-Cholor/Pods-Cholor-frameworks.sh
+++ Cholor/Pods/Target Support Files/Pods-Cholor/Pods-Cholor-frameworks.sh
@@ -141,6 +141,15 @@ strip_invalid_archs() {
   STRIP_BINARY_RETVAL=1
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Hero/Hero.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Spring/Spring.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Hero/Hero.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Spring/Spring.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
